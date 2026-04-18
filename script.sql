16182703fa0fd1ec5fcf12e422c8319de107b5cf
Davi <davi10felixs@gmail.com>
2026-04-18 13:26:56 -0300

    Exclusão de registro da tabela

--- script.sql
+++ script.sql
@@ -7,3 +7,10 @@ CREATE TABLE tb_alunos (
     data_nascimento DATETIME NOT NULL
 
 );
+
+INSERT INTO treina_recife.tb_alunos
+('id' , 'nome' , 'e-mail' , 'data_nascimento')
+VALUES 
+(1 , "Davi Félix." , "davi10@gmail.com", TIMESTAMP '2008')
+DELETE FROM treina_recife.tb_alunos WHERE id=1;
+DROP TABLE treina_recife.tb_alunos
