69b36ef9981dd80ff935e6157960d9a6a11b3a1d
Davi <davi10felixs@gmail.com>
2026-04-18 10:29:37 -0300

    Criação do README

--- olaMundo.java
+++ olaMundo.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class olaMundo {
 
 public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
     System.out.println("Olá mundo.");
-        
+         System.out.print("Digite seu nome: ");
+         String nome = scanner.nextLine();
+
+         scanner.close();
+
+         System.out.printf("Olá %s!" , nome);
     }
 }
-
